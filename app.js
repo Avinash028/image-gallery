c100c2f57bf4b419c0e5ad617c1fb2ec5f65efcb
Avinash028 <32132027+Avinash028@users.noreply.github.com>
2020-05-27 11:16:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,27 +10,26 @@ const methodOverride = require('method-override');
 
 const app = express();
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
-// Mongo URI
+
 const mongoURI = 'mongodb+srv://avinash:avinash@cluster0-fmft1.mongodb.net/test?retryWrites=true&w=majority';
 
-// Create mongo connection
 const conn = mongoose.createConnection(mongoURI);
 
-// Init gfs
+
 let gfs;
 
 conn.once('open', () => {
-  // Init stream
+  
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
 });
 
-// Create storage engine
+
 const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
@@ -51,11 +50,10 @@ const storage = new GridFsStorage({
 });
 const upload = multer({ storage });
 
-// @route GET /
-// @desc Loads form
+
 app.get('/', (req, res) => {
   gfs.files.find().toArray((err, files) => {
-    // Check if files
+   
     if (!files || files.length === 0) {
       res.render('index', { files: false });
     } else {
@@ -74,58 +72,54 @@ app.get('/', (req, res) => {
   });
 });
 
-// @route POST /upload
-// @desc  Uploads file to DB
+
 app.post('/upload', upload.single('file'), (req, res) => {
-  // res.json({ file: req.file });
+ 
   res.redirect('/');
 });
 
-// @route GET /files
-// @desc  Display all files in JSON
+
 app.get('/files', (req, res) => {
   gfs.files.find().toArray((err, files) => {
-    // Check if files
+    
     if (!files || files.length === 0) {
       return res.status(404).json({
         err: 'No files exist'
       });
     }
 
-    // Files exist
+    
     return res.json(files);
   });
 });
 
-// @route GET /files/:filename
-// @desc  Display single file object
+
 app.get('/files/:filename', (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
+   
     if (!file || file.length === 0) {
       return res.status(404).json({
         err: 'No file exists'
       });
     }
-    // File exists
+    
     return res.json(file);
   });
 });
 
-// @route GET /image/:filename
-// @desc Display Image
+
 app.get('/image/:filename', (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
+   
     if (!file || file.length === 0) {
       return res.status(404).json({
         err: 'No file exists'
       });
     }
 
-    // Check if image
+    
     if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
-      // Read output to browser
+      
       const readstream = gfs.createReadStream(file.filename);
       readstream.pipe(res);
     } else {
@@ -136,8 +130,7 @@ app.get('/image/:filename', (req, res) => {
   });
 });
 
-// @route DELETE /files/:id
-// @desc  Delete file
+
 app.delete('/files/:id', (req, res) => {
   gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
     if (err) {
